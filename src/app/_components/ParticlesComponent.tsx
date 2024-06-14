22ffeb2b7172cd2db45a782cc80fc954dcc0b6d6
amyhuangxbd <huangxbd1990@126.com>
2024-06-14 18:01:09 +0800

    fix error

--- src/app/_components/ParticlesComponent.tsx
+++ src/app/_components/ParticlesComponent.tsx
@@ -1,10 +1,6 @@
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Particles, { initParticlesEngine } from "@tsparticles/react";
-import type { Container, Engine } from "@tsparticles/engine";
-// import { loadAll } from "@/tsparticles/all"; // if you are going to use `loadAll`, install the "@tsparticles/all" package too.
-// import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
 import { loadSlim } from "@tsparticles/slim"; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
-// import { loadBasic } from "@tsparticles/basic"; // if you are going to use `loadBasic`, install the "@tsparticles/basic" package too.
 import '@/app/styles/particles.css'
 
 const ParticlesComponent = () => {
@@ -16,36 +12,17 @@ const ParticlesComponent = () => {
             // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
             // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
             // starting from v2 you can add only the features you need reducing the bundle size
-            //await loadAll(engine);
-            //await loadFull(engine);
             await loadSlim(engine);
-            //await loadBasic(engine);
         }).then(() => {
             setInit(true);
         });
     }, []);
 
-    // const particlesLoaded = (container) => {
-    //     console.log(container);
-    // };
-
     return (
          init ? <Particles
             id="tsparticles"
-            // particlesLoaded={particlesLoaded}
             options={{
-                // color: ["#03dac6", "#ff0266", "#000000"],
                 connectParticles: true,
-                // responsive: [
-                //     {
-                //     breakpoint: 768,
-                //     options: {
-                //         color: ["#faebd7", "#03dac6", "#ff0266"],
-                //         maxParticles: 43,
-                //         connectParticles: false
-                //     }
-                //     }
-                // ],
                 background: {
                     color: {
                         value: '#060b20',
@@ -57,30 +34,7 @@ const ParticlesComponent = () => {
                 },
                 fpsLimit: 120,
                 fullScreen: false,
-                // interactivity: {
-                //     events: {
-                //         onClick: {
-                //             enable: true,
-                //             mode: "push",
-                //         },
-                //         onHover: {
-                //             enable: true,
-                //             mode: "repulse",
-                //         },
-                //         resize: true,
-                //     },
-                //     modes: {
-                //         push: {
-                //             quantity: 4,
-                //         },
-                //         repulse: {
-                //             distance: 200,
-                //             duration: 0.4,
-                //         },
-                //     },
-                // },
                 particles: {
-                    // color: ["#03dac6", "#ff0266", "#000000"],
                     color: {
                         value: ["#03dac6", "#ff0266", "#000000"],
                     },
@@ -116,12 +70,6 @@ const ParticlesComponent = () => {
                     opacity: {
                         value: 0.5,
                     },
-                    // shape: {
-                    //     type: "circle",
-                    // },
-                    // size: {
-                    //     value: { min: 1, max: 5 },
-                    // },
                 },
                 detectRetina: true,
             }}
