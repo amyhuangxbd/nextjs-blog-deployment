dad9932ed233141f0630ec6cb6d22a65b67dad52
amyhuangxbd <huangxbd1990@126.com>
2024-06-14 17:43:15 +0800

    style

---
+++ src/app/_components/technology.tsx
@@ -0,0 +1,9 @@
+import Terials from '@/app/threejs/terials/page';
+const Technology = () => {
+    return ( <div className=' relative'>
+        <Terials />
+        <div className=' w-full absolute z-20 top-20 text-center text-5xl'>A Frontend /WebGL Developer</div>
+    </div> );
+};
+
+export default Technology;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,13 @@
-import Container from "@/app/_components/container";
-import { HeroPost } from "@/app/_components/hero-post";
-import { Intro } from "@/app/_components/intro";
-import { MoreStories } from "@/app/_components/more-stories";
-import { getAllPosts } from "@/lib/api";
 import HeroHeader from '@/app/_components/hero-header';
-import AvatarIntro from "./_components/avatar-intro";
-// import Link from "next/link";
-// import StickyNavMenu from "./_components/sticky-nav-menu";
-// import ThreeScene from './_components/ThreeScene'
+import Coder from "./_components/coder";
+import Technology from '@/app/_components/technology'
 
 export default function Index() {
-  const allPosts = getAllPosts();
-
-  const heroPost = allPosts[0];
-
-  const morePosts = allPosts.slice(1);
   return (
-    <main>
+    <main style={{ width: '100%', height: '100%' }}>
       <HeroHeader />
-      <AvatarIntro />
-      {/* <Container>
-        <Intro />
-        <HeroPost
-          title={heroPost.title}
-          coverImage={heroPost.coverImage}
-          date={heroPost.date}
-          author={heroPost.author}
-          slug={heroPost.slug}
-          excerpt={heroPost.excerpt}
-        />
-        {morePosts.length > 0 && <MoreStories posts={morePosts} />}
-      </Container> */}
+      <Coder />
+      <Technology />
     </main>
   );
 }
